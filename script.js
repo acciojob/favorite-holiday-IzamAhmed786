196bc487fcb6abdeb353e3a642eaa2e8858e2053
IzamAhmed786 <izamahmed786@gmail.com>
2026-02-13 15:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+public static void printAMazePaths(int i, int j, int N, int M, String psf) {
+    // Base case
+    if (i == N && j == M) {
+        System.out.println(psf);
+        return;
+    }
+
+    // Move horizontally if within bounds
+    if (j < M) {
+        printAMazePaths(i, j + 1, N, M, psf + "h");
+    }
+
+    // Move vertically if within bounds
+    if (i < N) {
+        printAMazePaths(i + 1, j, N, M, psf + "v");
+    }
+}
